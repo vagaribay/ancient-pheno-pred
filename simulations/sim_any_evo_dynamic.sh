8e83209554157989a6eff9d0ef885537d3a844cc
Valeria Añorve-Garibay <112778297+vagaribay@users.noreply.github.com>
2024-06-24 11:24:41 -0600

    Create sim_any_evo_dynamic.sh

---
+++ simulations/sim_any_evo_dynamic.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+TYPE=$1
+
+if [ "$TYPE" == "neutral_evolution" ]; then
+  echo "Running code for $TYPE"
+  #nrun=${SGE_TASK_ID}
+  nrun=$4
+  path=$3
+  slim -d hsq=$2 -d nrun=${nrun} -d path="'${path}'" neutral_evolution.slim > ${path}/OUT.${nrun}
+
+elif [ "$TYPE" == "stabilizing_selection" ]; then
+  echo "Running code for $TYPE"
+  #nrun=${SGE_TASK_ID}
+  nrun=$5
+  path=$4
+  slim -d hsq=$2 -d w=$3 -d nrun=${nrun} -d path="'${path}'" stabilizing_selection.slim > ${path}/OUT.${nrun}
+
+elif [ "$TYPE" == "directional_selection" ]; then
+  echo "Running code for $TYPE"
+  #nrun=${SGE_TASK_ID}
+  nrun=$5
+  path=$4
+  slim -d hsq=$2 -d QTL_sd=$3 -d nrun=${nrun} -d path="'${path}'" directional_selection.slim > ${path}/OUT.${nrun}
+  
+fi

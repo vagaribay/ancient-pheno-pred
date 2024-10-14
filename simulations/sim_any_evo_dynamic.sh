4de4fb210ddaf69112d53bee325a6d17fe6c988c
Valeria Añorve-Garibay <112778297+vagaribay@users.noreply.github.com>
2024-10-14 13:04:48 -0400

    Update sim_any_evo_dynamic.sh

--- simulations/sim_any_evo_dynamic.sh
+++ simulations/sim_any_evo_dynamic.sh
@@ -11,20 +11,22 @@ TYPE=$1
 
 if [ "$TYPE" == "neutral_evolution" ]; then
   echo "Running code for $TYPE"
-  nrun=${SGE_TASK_ID}
+  nrun=${SLURM_ARRAY_TASK_ID}
   path=$3
   slim -d hsq=$2 -d nrun=${nrun} -d path="'${path}'" neutral_evolution.slim > ${path}/OUT.${nrun}
 
 elif [ "$TYPE" == "stabilizing_selection" ]; then
   echo "Running code for $TYPE"
-  nrun=${SGE_TASK_ID}
+  nrun=${SLURM_ARRAY_TASK_ID}
   path=$4
   slim -d hsq=$2 -d w=$3 -d nrun=${nrun} -d path="'${path}'" stabilizing_selection.slim > ${path}/OUT.${nrun}
 
 elif [ "$TYPE" == "directional_selection" ]; then
   echo "Running code for $TYPE"
-  nrun=${SGE_TASK_ID}
+  nrun=${SLURM_ARRAY_TASK_ID}
   path=$4
   slim -d hsq=$2 -d QTL_sd=$3 -d nrun=${nrun} -d path="'${path}'" directional_selection.slim > ${path}/OUT.${nrun}
   
 fi
+
+sbatch --array=1 sim_any_evo_dynamic.sh neutral_evolution 0.5 /users/vanorveg/data/vanorveg/ancient-pheno-pred/output/neutral_evolution/

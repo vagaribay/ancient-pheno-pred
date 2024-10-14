f19d2ff6599b1f6209a48d655a50c4e2715cdc33
Valeria Añorve-Garibay <112778297+vagaribay@users.noreply.github.com>
2024-10-14 12:59:57 -0400

    Update sim_any_evo_dynamic.sh

--- simulations/sim_any_evo_dynamic.sh
+++ simulations/sim_any_evo_dynamic.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#SBATCH -t 10:00:00
+#SBATCH --mem=2g
+module load slim/4.0.1-kymgtmu
 # code to simulate any evolutionary dynamic based on SLiM scripts
 # TYPE defines the type of evolutionary scenario simulated.
 # The three options are "neutral_evolution", "stabilizing_selection" and "directional_selection" which defines 
@@ -8,22 +11,19 @@ TYPE=$1
 
 if [ "$TYPE" == "neutral_evolution" ]; then
   echo "Running code for $TYPE"
-  #nrun=${SGE_TASK_ID}
-  nrun=$4
+  nrun=${SGE_TASK_ID}
   path=$3
   slim -d hsq=$2 -d nrun=${nrun} -d path="'${path}'" neutral_evolution.slim > ${path}/OUT.${nrun}
 
 elif [ "$TYPE" == "stabilizing_selection" ]; then
   echo "Running code for $TYPE"
-  #nrun=${SGE_TASK_ID}
-  nrun=$5
+  nrun=${SGE_TASK_ID}
   path=$4
   slim -d hsq=$2 -d w=$3 -d nrun=${nrun} -d path="'${path}'" stabilizing_selection.slim > ${path}/OUT.${nrun}
 
 elif [ "$TYPE" == "directional_selection" ]; then
   echo "Running code for $TYPE"
-  #nrun=${SGE_TASK_ID}
-  nrun=$5
+  nrun=${SGE_TASK_ID}
   path=$4
   slim -d hsq=$2 -d QTL_sd=$3 -d nrun=${nrun} -d path="'${path}'" directional_selection.slim > ${path}/OUT.${nrun}
   

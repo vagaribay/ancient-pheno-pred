3e51b77c28934abac79d0964e2d1bf2f703a777d
Valeria Añorve-Garibay <112778297+vagaribay@users.noreply.github.com>
2025-02-07 11:48:13 -0500

    Update sim_any_evo_dynamic.sh

--- simulations/sim_any_evo_dynamic.sh
+++ simulations/sim_any_evo_dynamic.sh
@@ -6,7 +6,18 @@ module load slim/4.0.1-kymgtmu
 # TYPE defines the type of evolutionary scenario simulated.
 # The three options are "neutral_evolution", "stabilizing_selection" and "directional_selection" which defines 
 # if the trait is evolving under neutral evolution, stabilizing selection or directional selection, respectively.
-
+# hsq stands for heritability value, assign accordingly
+# path corresponds to the desired output dir
+# nrun assigns a run ID for the specific replicate, e.g. sim_``1``.slim based on SLURM_ARRAY_TASK_ID
+# w represents the strength of selection, assign accordingly. we used w={1,2,3,4,5} to represent cases going from stronger to weaker stabilizing selection
+# QTL_sd represents the standard deviation of the QTLs distribution (normal with mean 0 and sd QTL_sd)
+# e.g. slurm command
+# neutral evolution with h2 = 1
+# sbatch --array=1 sim_any_evo_dynamic.sh neutral_evolution 1.0 neutral_evolution/
+# stabilizing selection with h2 = 1 and w = 1
+# sbatch --array=1 sim_any_evo_dynamic.sh stabilizing_selection 1.0 1.0 stabilizing_selection/
+# directional selection with h2 = 1 and QTLs sd = 0.25
+# sbatch --array=1 sim_any_evo_dynamic.sh directional_selection 1.0 0.25
 TYPE=$1
 
 if [ "$TYPE" == "neutral_evolution" ]; then
@@ -29,7 +40,7 @@ elif [ "$TYPE" == "directional_selection" ]; then
   
 fi
 
-sbatch --array=1-100 sim_any_evo_dynamic.sh neutral_evolution 1.0 /users/vanorveg/data/vanorveg/ancient-pheno-pred/output/neutral_evolution/total/
+
 sbatch --array=1-100 sim_any_evo_dynamic.sh neutral_evolution 0.5 /users/vanorveg/data/vanorveg/ancient-pheno-pred/output/neutral_evolution/mid/
 
 
@@ -40,7 +51,7 @@ sbatch --array=1 sim_any_evo_dynamic.sh directional_selection 0.5 0.25 /users/va
 sbatch --array=1 sim_any_evo_dynamic.sh directional_selection 0.5 0.0025 /users/vanorveg/data/vanorveg/ancient-pheno-pred/output/directional_selection/sd_00025/
 
 
-sbatch --array=1-100 sim_any_evo_dynamic.sh stabilizing_selection 1.0 1.0 /users/vanorveg/data/vanorveg/ancient-pheno-pred/output/stabilizing_selection/total/1/
+
 sbatch --array=1-100 sim_any_evo_dynamic.sh stabilizing_selection 1.0 2.0 /users/vanorveg/data/vanorveg/ancient-pheno-pred/output/stabilizing_selection/total/2/
 sbatch --array=1-100 sim_any_evo_dynamic.sh stabilizing_selection 1.0 3.0 /users/vanorveg/data/vanorveg/ancient-pheno-pred/output/stabilizing_selection/total/3/
 sbatch --array=1-100 sim_any_evo_dynamic.sh stabilizing_selection 1.0 4.0 /users/vanorveg/data/vanorveg/ancient-pheno-pred/output/stabilizing_selection/total/4/
@@ -56,7 +67,7 @@ sbatch --array=1-100 sim_any_evo_dynamic.sh stabilizing_selection 0.5 5.0 /users
 
 
 sbatch --array=33,55,8 sim_any_evo_dynamic.sh directional_selection 1.0 0.25 /users/vanorveg/data/vanorveg/ancient-pheno-pred/output/directional_selection/total/025/
-sbatch --array=79,101-105 sim_any_evo_dynamic.sh directional_selection 1.0 0.025 /users/vanorveg/data/vanorveg/ancient-pheno-pred/output/directional_selection/total/0025/
+
 sbatch --array=24,34,6,68,8,90,98,101-105 sim_any_evo_dynamic.sh directional_selection 1.0 0.0025 /users/vanorveg/data/vanorveg/ancient-pheno-pred/output/directional_selection/total/00025/
 
 sbatch --array=93 sim_any_evo_dynamic.sh directional_selection 0.5 0.25 /users/vanorveg/data/vanorveg/ancient-pheno-pred/output/directional_selection/mid/025/
